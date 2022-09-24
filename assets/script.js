52b7ade5d0d2a0bfee8eba96b863f068fab6778b
Tim Mardis <mardistj96@yahoo.com>
2022-09-24 11:38:07 -0500

    Improved Google API functionality

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,32 @@
-function initMap() {
-    const uluru = { lat: -25.344, lng: 131.031 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
+var map;
+var service;
+var infowindow;
+var gobutton = document.getElementById("gobutton")
 
-  console.log("hello world")
+gobutton.addEventListener("click", function initMap() {
+
+  var userquery = document.getElementById("user-search").value
+
+  console.log(userquery)
+
+  infowindow = new google.maps.InfoWindow();
+
+  map = new google.maps.Map(
+      document.getElementById('map'), {});
+
+  var request = {
+    query: userquery,
+    fields: ['name', 'geometry', 'formatted_address'],
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+
+  service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        console.log(results[i]);
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+})
