db1256dcaa3bd6f7cfb97fdb35a676a7c56706a6
Tim Mardis <mardistj96@yahoo.com>
2022-09-25 13:18:30 -0500

    Search bar will search nearby restaurants

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,57 @@
 var map;
 var service;
 var infowindow;
+var geocoder;
 var gobutton = document.getElementById("gobutton")
+var userlocation;
+var displaylist = document.getElementById("list")
 
-gobutton.addEventListener("click", function initMap() {
+function initMap() {
 
-  var userquery = document.getElementById("user-search").value
-
-  console.log(userquery)
+  console.log(userlocation[0].geometry.location)
 
   infowindow = new google.maps.InfoWindow();
 
   map = new google.maps.Map(
-      document.getElementById('map'), {});
+      document.getElementById('map'));
 
   var request = {
-    query: userquery,
-    fields: ['name', 'geometry', 'formatted_address'],
+    location: userlocation[0].geometry.location,
+    radius: '200',
+    type: ['restaurant']
   };
 
   var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 
-  service.findPlaceFromQuery(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < results.length; i++) {
+        var listname = document.createElement('h1');
+        listname.textContent = results[i].name;
+        var listaddress = document.createElement('li');
+        listaddress.textContent = results[i].vicinity;
+        displaylist.appendChild(listname);
+        listname.appendChild(listaddress);
         console.log(results[i]);
       }
-      map.setCenter(results[0].geometry.location);
+    }
+  }
+}
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  map = new google.maps.Map(document.getElementById('map'));
+}
+
+gobutton.addEventListener("click", function codeAddress() {
+  var address = document.getElementById('user-search').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+    userlocation = results
+    initMap()
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
     }
   });
 })
